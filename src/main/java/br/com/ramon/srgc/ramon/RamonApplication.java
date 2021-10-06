478b0de94ce091913f683a2de0795fd9c0ea01db
Ramon Gimenes <ramongimee@hotmail.com>
2021-10-06 01:21:15 -0300

    Update RamonApplication.java

--- src/main/java/br/com/ramon/srgc/ramon/RamonApplication.java
+++ src/main/java/br/com/ramon/srgc/ramon/RamonApplication.java
@@ -3,7 +3,7 @@ package br.com.ramon.srgc.ramon;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class, DataSourceTransactionManagerAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 @SpringBootApplication
 public class RamonApplication {
 
